3da5904c8cba84cb9e4b2d92224bab32b71877fe
bethany-c <70467659+bethany-c@users.noreply.github.com>
2021-01-27 17:32:50 -0700

    Update card-school-component.jsx

--- src/components/card/card-school-component.jsx
+++ src/components/card/card-school-component.jsx
@@ -4,10 +4,7 @@ import './card-component-style.css'
 
 export const SchoolCard = ({ monster }) => ( 
     <div className='card-container'>
-    <img
-        alt='monster'
-        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-    />
+    
     <h2> {monster.name} </h2>
     <p> {monster.schoolCode} </p>
 
