59734616e34d366db6d06a0ba83ddb30fbe5d8f8
bethany-c <70467659+bethany-c@users.noreply.github.com>
2021-01-27 17:24:08 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './App.css';
+import Teachers from './pages/Teachers';
+import Register from './pages/Register';
+import Schools from './pages/Schools';
+import { Link } from 'react-router-dom';
+
+
+
+
+const App = () => {
+  const [page, setPage] = useState('teachers'); //hold data from API
+  
+  return (
+    <div className='App'>
+      <button
+        onClick={() => setPage('teachers')}
+      >
+        Teachers
+      </button>
+      <button
+        onClick={() => setPage('register')}
+      >
+        Register
+      </button>
+      <button
+        onClick={() => setPage('schools')}
+      >
+        Schools
+      </button>
+      {page == 'teachers' ? <Teachers/> : page == 'register' ? <Register/> : <Schools/>}
+    </div>
+  );
+}
+export default App;
