5412a7e43e71510829d237aa0e131dd007565c86
bethany-c <70467659+bethany-c@users.noreply.github.com>
2021-01-27 17:32:28 -0700

    Update card-component.jsx

--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -4,10 +4,7 @@ import './card-component-style.css'
 
 export const Card = ({ monster }) => ( 
     <div className='card-container'>
-    <img
-        alt='monster'
-        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-    />
+    
     <h2> {monster.firstName} </h2>
     <p> {monster.lastName} </p>
     <p> {monster.school.name}</p>
