e62f18e2489b2ee60e7e131ccae1ea8fb7e689bc
bethany-c <70467659+bethany-c@users.noreply.github.com>
2021-01-28 16:47:24 -0700

    Update confirm-delete.jsx

--- src/components/search-box/confirm-delete.jsx
+++ src/components/search-box/confirm-delete.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { DeleteBox } from './delete-box-component';
 
-export const DeleteConfirm = ({ DeleteBox }) => (
+export const DeleteConfirm = ({ DeleteBox,onDeleteChange }) => (
     <button
     className='search-box'
-    placeholder='delete teacher ID'
-    onChange={DeleteBox}
+    placeholder='delete'
+    value={onDeleteChange}
+    onClick={DeleteBox}
     />
     
 );
