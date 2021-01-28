ec51c6cf3801f1c213b16e29799c59bb24c0d5b9
bethany-c <70467659+bethany-c@users.noreply.github.com>
2021-01-27 17:27:18 -0700

    Add files via upload

---
+++ src/components/search-box/delete-box-component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-box-style.css';
+
+export const DeleteBox = ({ onDeleteChange }) => (
+    <input
+    className='search-box'
+    placeholder='delete teacher ID'
+    onChange={onDeleteChange}
+    />
+    
+);
