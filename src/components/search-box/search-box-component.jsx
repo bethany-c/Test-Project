d5ca5aaa91f92affb35505c9773acc383b14621a
bethany-c <70467659+bethany-c@users.noreply.github.com>
2021-01-28 21:51:51 -0700

    Update search-box-component.jsx

--- src/components/search-box/search-box-component.jsx
+++ src/components/search-box/search-box-component.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './search-box-style.css';
 
-
 export const SearchBox = ({ onSearchChange }) => (
     <input
-    className='search-box'
-    type='search'
-    placeholder='search teacher ID'
-    onChange={onSearchChange}
+        className='search-box'
+        type='search'
+        placeholder='search teacher ID'
+        onChange={onSearchChange}
     />
 );
