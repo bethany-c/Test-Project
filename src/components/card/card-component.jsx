6ba42fc0bcde7d4f445fe2f844a368766d8d6cc1
bethany-c <70467659+bethany-c@users.noreply.github.com>
2021-01-28 21:50:06 -0700

    Update card-component.jsx

--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './card-component-style.css'
 
-
 export const Card = ({ monster }) => ( 
     <div className='card-container'>
-    
-    <h2> {monster.firstName} </h2>
-    <p> {monster.lastName} </p>
-    <p> {monster.id}</p>
-    <p> {monster.school.name}</p>
+        <h2> {monster.firstName} </h2>
+        <p> {monster.lastName} </p>
+        <p> {monster.id}</p>
+        <p> {monster.school.name}</p>
     </div>
 );
